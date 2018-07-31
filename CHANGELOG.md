c77cc4739d2382d7ba847b460a25f47392e663c2
Kevin Moore <kevmoo@google.com>
2018-07-31 15:14:13 -0700

    support dart 2 stable, release 1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.2
+
+* Support Dart 2 stable.
+
 ## 1.0.1
 
 * readme and changelog
--- bin/jfmt.dart
+++ bin/jfmt.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 main(List<String> args) async {
   var input =
-      await stdin.transform(UTF8.decoder).transform(JSON.decoder).single;
+      await stdin.transform(utf8.decoder).transform(json.decoder).single;
 
   print(const JsonEncoder.withIndent(' ').convert(input));
 }
